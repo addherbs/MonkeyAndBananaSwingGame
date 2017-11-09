9da98068443847fc0d0827dfc13e21fb5f10e917
Aditya Pandey <addherbs@gmail.com>
2017-11-08 19:52:07 -0600

    Added constants

---
+++ src/com/adipandey/monkeybanana/Constants.java
@@ -0,0 +1,20 @@
+package com.adipandey.monkeybanana;
+
+public class Constants {
+
+	public final static int BOX = 20;
+
+	public static   int TOTAL_TIME = 40;
+
+	public static   int ROUND_TIME = 10;
+
+	public static  int REMAINING = 31;
+
+	public final static int WIDTH = 600;
+
+	public final static int HEIGHT =600;
+
+	public final static int TOTAL_BANANAS = 10;
+	
+	public final static int IMAGE = 20;
+}
